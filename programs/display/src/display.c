ebc3c63adb825f14bdf25aaa096ada6b0f458990
My Name <foo@lisp.us.to>
2017-02-15 23:59:10 -0500

    Making unified interface for framebuffer and vga textmode output

--- programs/display/src/display.c
+++ programs/display/src/display.c
@@ -13,9 +13,10 @@ enum {
 };
 
 enum {
-	WIDTH  = 80,
-	START  = 0,
-	HEIGHT = 25,
+	VGA_TEXT_WIDTH  = 80,
+	VGA_TEXT_HEIGHT = 25,
+	VGA_TEXT_START  = 0,
+	VGA_TEXT_COLOR  = 0x17,
 };
 
 typedef struct vga_char {
@@ -23,11 +24,25 @@ typedef struct vga_char {
 	uint8_t color;
 } vga_char_t;
 
-typedef struct vga_state {
-	vga_char_t *textbuf;
+typedef struct display {
+	union {
+		vga_char_t *textbuf;
+		uint32_t   *pixelbuf;
+	};
+
+	void (*draw_char)( struct display *state, unsigned x, unsigned y, char c );
+	void (*clear)( struct display *state );
+	void (*scroll)( struct display *state );
+
 	unsigned x;
 	unsigned y;
-} vga_state_t;
+
+	// width and height are in characters, for the framebuffer
+	// this will be calculated by dividing screen dimensions by
+	// font dimensions
+	unsigned width;
+	unsigned height;
+} display_t;
 
 unsigned hash_string( const char *str ){
 	unsigned hash = 757;
@@ -61,55 +76,141 @@ static inline unsigned nameserver_lookup( unsigned server, const char *name ){
 	return msg.data[0];
 }
 
-static inline void scroll_display( vga_state_t *state ){
-	for ( unsigned i = START + 1; i < HEIGHT; i++ ){
-		vga_char_t *foo = state->textbuf + WIDTH * (i - 1);
-		vga_char_t *bar = state->textbuf + WIDTH * i;
-
-		for ( unsigned k = 0; k < WIDTH; k++ ){
-			*foo++ = *bar++;
-		}
-
-		for ( unsigned k = 0; k < WIDTH; k++ ){
-			foo->text = ' ';
-			foo++;
-		}
-	}
-}
-
-static inline void do_newline( vga_state_t *state ){
-	if ( ++state->y >= HEIGHT - 1 ){
-		scroll_display( state );
-		state->y = HEIGHT - 1;
+static inline void do_newline( display_t *state ){
+	if ( ++state->y >= state->height - 1 ){
+		state->scroll( state );
+		state->y = state->height - 1;
 	}
 
 	state->x = 0;
 }
 
 static bootinfo_t *c4_bootinfo = BOOTINFO_ADDR;
-static void framebuffer_init( void );
+static void framebuffer_init( display_t *state );
+static void textbuffer_init( display_t *state );
 
-void _start( uintptr_t nameserver ){
-	static const uint8_t color = 0x17;
-	message_t msg;
+static inline void framebuf_draw_char( display_t *state,
+                                       unsigned x,
+                                       unsigned y,
+                                       char c )
+{
+	c4_debug_putchar( c );
+}
 
-	framebuffer_init();
+static inline void framebuf_clear( display_t *state ){
 
-	vga_state_t state = {
-		.textbuf = (void *)0xb8000,
-		.x       = 0,
-		.y       = START,
+}
+
+static inline void framebuf_scroll( display_t *state ){
+
+}
+
+static void framebuffer_init( display_t *state ){
+	unsigned size =
+		c4_bootinfo->framebuffer.width *
+		c4_bootinfo->framebuffer.height *
+		4;
+
+	*state = (display_t){
+		.pixelbuf = (void *)0xfb000000,
+		.x        = 0,
+		.y        = 0,
+		.width    = 80,
+		.height   = 25,
+
+		.draw_char = framebuf_draw_char,
+		.scroll    = framebuf_scroll,
+		.clear     = framebuf_clear,
 	};
 
-	nameserver_bind( nameserver, "/dev/console" );
+	c4_request_physical( 0xfb000000,
+	                     c4_bootinfo->framebuffer.addr,
+	                     size / PAGE_SIZE + 1,
+	                     PAGE_READ | PAGE_WRITE );
+
+	uint32_t *fb = (void *)0xfb000000;
+
+	for ( unsigned y = 0; y < c4_bootinfo->framebuffer.height; y++ ){
+		for ( unsigned x = 0; x < c4_bootinfo->framebuffer.width; x++ ){
+			unsigned index = y * c4_bootinfo->framebuffer.width + x;
+
+			fb[index] = ((y & 0xff) << 16) | ((x & 0xff) << 8) | (x ^ y);
+		}
+	}
+}
+
+static inline void text_draw_char( display_t *state,
+                                   unsigned x,
+                                   unsigned y,
+                                   char c )
+{
+	vga_char_t *temp = state->textbuf + state->width * state->y + state->x;
+
+	temp->text = c;
+	temp->color = VGA_TEXT_COLOR;
+}
+
+static inline void text_scroll( display_t *state ){
+	for ( unsigned i = 1; i < state->height; i++ ){
+		vga_char_t *foo = state->textbuf + state->width * (i - 1);
+		vga_char_t *bar = state->textbuf + state->width * i;
+
+		for ( unsigned k = 0; k < state->width; k++ ){
+			*foo++ = *bar++;
+		}
+
+		for ( unsigned k = 0; k < state->width; k++ ){
+			foo->text = ' ';
+			foo++;
+		}
+	}
+}
+
+static void text_clear( display_t *state ){
+	state->x = 0;
+	state->y = 0;
+
+	for ( unsigned i = 0; i < state->width * state->height; i++ ){
+		state->textbuf[i].text  = ' ';
+		state->textbuf[i].color = VGA_TEXT_COLOR;
+	}
+}
+
+static void textbuffer_init( display_t *state ){
+	*state = (display_t){
+		.textbuf = (void *)0xb8000,
+		.x       = 0,
+		.y       = 0,
+		.width   = VGA_TEXT_WIDTH,
+		.height  = VGA_TEXT_HEIGHT,
+
+		.draw_char = text_draw_char,
+		.clear     = text_clear,
+		.scroll    = text_scroll,
+	};
 
 	// request access to the vga text buffer
 	c4_request_physical( 0xb8000, 0xb8000, 1, PAGE_READ | PAGE_WRITE );
 
-	for ( unsigned i = 0; i < WIDTH * HEIGHT; i++ ){
-		state.textbuf[i].text = ' ';
-		state.textbuf[i].color = color;
+	for ( unsigned i = 0; i < VGA_TEXT_WIDTH * VGA_TEXT_HEIGHT; i++ ){
+		state->textbuf[i].text = ' ';
+		state->textbuf[i].color = VGA_TEXT_COLOR;
 	}
+
+}
+
+void _start( uintptr_t nameserver ){
+	message_t msg;
+	display_t state;
+
+	if ( c4_bootinfo->framebuffer.exists ){
+		framebuffer_init( &state );
+
+	} else {
+		textbuffer_init( &state );
+	}
+
+	nameserver_bind( nameserver, "/dev/console" );
 
 	while ( true ){
 		c4_msg_recieve( &msg, 0 );
@@ -121,42 +222,14 @@ void _start( uintptr_t nameserver ){
 
 		} else if ( c == '\b' ){
 			state.x--;
-
-			vga_char_t *temp = state.textbuf + WIDTH * state.y + state.x;
-			temp->text = ' ';
+			state.draw_char( &state, state.x, state.y, ' ' );
 
 		} else {
-			vga_char_t *temp = state.textbuf + WIDTH * state.y + state.x;
 
-			temp->text  = c;
-			temp->color = color;
+			state.draw_char( &state, state.x, state.y, c );
 
-			if ( state.x++ >= WIDTH ){
+			if ( state.x++ >= state.width ){
 				do_newline( &state );
-			}
-		}
-	}
-}
-
-static void framebuffer_init( void ){
-	if ( c4_bootinfo->framebuffer.exists ){
-		unsigned size =
-			c4_bootinfo->framebuffer.width *
-			c4_bootinfo->framebuffer.height *
-			4;
-
-		c4_request_physical( 0xfb000000,
-		                     c4_bootinfo->framebuffer.addr,
-		                     size / PAGE_SIZE + 1,
-		                     PAGE_READ | PAGE_WRITE );
-
-		uint32_t *fb = (void *)0xfb000000;
-
-		for ( unsigned y = 0; y < c4_bootinfo->framebuffer.height; y++ ){
-			for ( unsigned x = 0; x < c4_bootinfo->framebuffer.width; x++ ){
-				unsigned index = y * c4_bootinfo->framebuffer.width + x;
-
-				fb[index] = ((y & 0xff) << 16) | ((x & 0xff) << 8) | (x ^ y);
 			}
 		}
 	}

3e7861d383a94e365cc22fd74f0dd432e88ae8e9
My Name <foo@lisp.us.to>
2017-03-07 21:55:46 -0500

    Removing WEAK attributes from forward declarations

--- libraries/c4rt/include/c4rt/stublibc.h
+++ libraries/c4rt/include/c4rt/stublibc.h
@@ -3,12 +3,12 @@
 #include <c4rt/compiler.h>
 #include <stddef.h>
 
-WEAK void *memcpy(void *dest, const void *src, size_t n);
-WEAK void *memset(void *s, int c, size_t n);
-WEAK size_t strlen(const char *s);
-WEAK char *strcpy(char *dest, const char *src);
-WEAK char *strncpy(char *dest, const char *src, size_t n);
-WEAK size_t strlcpy(char *dest, const char *src, size_t size);
-WEAK int strcmp( const char *s1, const char *s2 );
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+size_t strlen(const char *s);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
+size_t strlcpy(char *dest, const char *src, size_t size);
+int strcmp( const char *s1, const char *s2 );
 
 #endif

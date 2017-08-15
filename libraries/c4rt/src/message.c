24b81327722fa7a719f5f016142e869ebf250da1
My Name <foo@lisp.us.to>
2017-08-15 15:42:21 -0500

    Adding IPC endpoint creation functions

--- libraries/c4rt/src/message.c
+++ libraries/c4rt/src/message.c
@@ -1,5 +1,13 @@
 #include <c4rt/c4rt.h>
 
+int c4_msg_create_sync( void ){
+	int ret = 0;
+
+	DO_SYSCALL( SYSCALL_SYNC_CREATE, 0, 0, 0, 0, ret );
+
+	return ret;
+}
+
 int c4_msg_send( message_t *buffer, unsigned to ){
 	int ret = 0;
 
@@ -12,6 +20,14 @@ int c4_msg_recieve( message_t *buffer, unsigned from ){
 	int ret = 0;
 
 	DO_SYSCALL( SYSCALL_SYNC_RECIEVE, buffer, from, 0, 0, ret );
+
+	return ret;
+}
+
+int c4_msg_create_async( void ){
+	int ret = 0;
+
+	DO_SYSCALL( SYSCALL_SYNC_CREATE, 0, 0, 0, 0, ret );
 
 	return ret;
 }

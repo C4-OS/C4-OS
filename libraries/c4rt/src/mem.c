4449964ba0bc627f8177995b6175df171073d7ec
My Name <foo@lisp.us.to>
2017-08-15 18:03:51 -0500

    Implementing new cspace and address space syscall wrappers

--- libraries/c4rt/src/mem.c
+++ libraries/c4rt/src/mem.c
@@ -1,77 +1,68 @@
 #include <c4rt/c4rt.h>
 
+int c4_addrspace_create( void ){
+	int ret;
+	DO_SYSCALL( SYSCALL_ADDRSPACE_CREATE, 0, 0, 0, 0, ret );
+	return ret;
+}
+
+int c4_addrspace_map( uint32_t addrspace,
+                      uint32_t frame,
+                      uintptr_t address,
+                      unsigned permissions )
+{
+	int ret;
+	DO_SYSCALL( SYSCALL_ADDRSPACE_MAP, addrspace,
+	            frame, address, permissions, ret );
+	return ret;
+}
+
+int c4_addrspace_unmap( uint32_t addrspace, uintptr_t address ){
+	int ret;
+	DO_SYSCALL( SYSCALL_ADDRSPACE_UNMAP, addrspace, address, 0, 0, ret );
+	return ret;
+}
+
+int c4_phys_frame_split( uint32_t frame, size_t offset ){
+	int ret;
+	DO_SYSCALL( SYSCALL_PHYS_FRAME_SPLIT, frame, offset, 0, 0, ret );
+	return ret;
+}
+
+// Deprecated
 void *c4_request_physical( uintptr_t virt,
                            uintptr_t physical,
                            unsigned size,
                            unsigned permissions )
 {
-	message_t msg = {
-		.type = MESSAGE_TYPE_REQUEST_PHYS,
-		.data = {
-			virt,
-			physical,
-			size,
-			permissions
-		},
-	};
-
-	c4_msg_send( &msg, 0 );
-
-	return (void *)virt;
+	return NULL;
 }
 
+// Deprecated
 int c4_mem_unmap( unsigned thread_id, void *addr ){
-	message_t msg = {
-		.type = MESSAGE_TYPE_UNMAP,
-		.data = { (uintptr_t)addr, },
-	};
-
-	return c4_msg_send( &msg, thread_id );
+	return 0;
 }
 
+// Deprecated
 int c4_mem_map_to( unsigned thread_id,
                    void *from,
                    void *to,
                    unsigned size,
                    unsigned permissions )
 {
-	message_t msg = {
-		.type = MESSAGE_TYPE_MAP_TO,
-		.data = {
-			(uintptr_t)from,
-			(uintptr_t)to,
-			(uintptr_t)size,
-			(uintptr_t)permissions,
-		},
-	};
-
-	return c4_msg_send( &msg, thread_id );
+	return 0;
 }
 
+// Deprecated
 int c4_mem_grant_to( unsigned thread_id,
                      void *from,
                      void *to,
                      unsigned size,
                      unsigned permissions )
 {
-	message_t msg = {
-		.type = MESSAGE_TYPE_GRANT_TO,
-		.data = {
-			(uintptr_t)from,
-			(uintptr_t)to,
-			(uintptr_t)size,
-			(uintptr_t)permissions,
-		},
-	};
-
-	return c4_msg_send( &msg, thread_id );
+	return 0;
 }
 
+// Deprecated
 void c4_dump_maps( unsigned thread ){
-	message_t msg = {
-		.type = MESSAGE_TYPE_DUMP_MAPS,
-		.data = { thread },
-	};
-
-	c4_msg_send( &msg, thread );
 }

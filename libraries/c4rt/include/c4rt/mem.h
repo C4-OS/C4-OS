b8410a4b3d5229498a53e9fb0e3c0ed5e005b584
My Name <foo@lisp.us.to>
2017-10-01 23:15:18 -0400

    Starting to implement memory objects (broken build)

---
+++ libraries/c4rt/include/c4rt/mem.h
@@ -0,0 +1,36 @@
+#ifndef _C4RT_MEM_H
+#define _C4RT_MEM_H 1
+
+#include <c4rt/interface/pager.h>
+#include <c4/capability.h>
+
+typedef struct mem_object {
+	union {
+		uintptr_t vaddr;
+		void *vaddrptr;
+	};
+
+	uint32_t page_obj;
+	uint32_t permissions;
+	bool mapped;
+} c4_mem_object_t;
+
+static inline unsigned c4_page_to_cap_perms( unsigned page_perms ){
+	unsigned ret = CAP_MULTI_USE | CAP_SHARE;
+
+	if ( page_perms & PAGE_READ )  ret |= CAP_ACCESS;
+	if ( page_perms & PAGE_WRITE ) ret |= CAP_MODIFY;
+
+	return ret;
+}
+
+c4_mem_object_t c4_memobj_make( uint32_t obj, uintptr_t vaddr, unsigned permissions );
+bool c4_memobj_map( c4_mem_object_t *obj, uint32_t addrspace );
+bool c4_memobj_unmap( c4_mem_object_t *obj, uint32_t addrspace );
+//bool            c4_memobj_map( c4_mem_object_t *obj );
+//bool            c4_memobj_unmap( c4_mem_object_t *obj );
+void            c4_memobj_destroy( c4_mem_object_t *obj );
+int             c4_memobj_share( c4_mem_object_t *memobj, uint32_t endpoint );
+c4_mem_object_t c4_memobj_recieve( uintptr_t vaddr, uint32_t endpoint );
+
+#endif

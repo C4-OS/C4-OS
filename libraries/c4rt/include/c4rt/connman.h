af3d2ea9890c37e5529721aa26d20af10882f861
My Name <foo@lisp.us.to>
2018-08-16 17:50:18 -0700

    Implementing connection manager, solves select() problem

---
+++ libraries/c4rt/include/c4rt/connman.h
@@ -0,0 +1,56 @@
+#ifndef _C4RT_CONNMAN_H
+#define _C4RT_CONNMAN_H 1
+
+/* TODO: this could be made entirely asyncronous, right now it only
+ *       blocks on one syncronous message to the server port,
+ *       which makes things simpler since then only one server port is
+ *       needed. But, being fully asyncronous would be pretty useful,
+ *       so after everything is working it's definitely a thing
+ *       to consider
+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <c4rt/mem.h>
+#include <c4rt/ringbuffer.h>
+#include <c4rt/andtree.h>
+
+enum {
+	C4RT_CONNMAN_CONNECT = 0xdabd00d,
+	C4RT_CONNMAN_DISCONNECT,
+	C4RT_CONNMAN_CLIENT_ID,
+	C4RT_CONNMAN_NEW_MESSAGE,
+	C4RT_CONNMAN_WRITE,
+	C4RT_CONNMAN_READ,
+};
+
+// this struct is used both as the client handle, and the internal
+// client struct of the server, which keeps things simpler.
+typedef struct {
+	uint32_t server_port;
+	uint32_t async_port;
+	uint32_t client_id;
+
+	c4_mem_object_t bufobj;
+	c4_ringbuf_t *ringbuf;
+} c4rt_conn_t;
+
+typedef struct {
+	c4rt_andtree_t tree;
+	uint32_t server_port;
+} c4rt_conn_server_t;
+
+bool c4rt_connman_connect(c4rt_conn_t *conn, uint32_t server);
+void c4rt_connman_disconnect(c4rt_conn_t *conn);
+
+bool c4rt_connman_send(c4rt_conn_t *conn, message_t *msg);
+bool c4rt_connman_recv(c4rt_conn_t *conn, message_t *msg, bool block);
+size_t c4rt_connman_write(c4rt_conn_t *conn, void *ptr, size_t n);
+size_t c4rt_connman_read(c4rt_conn_t *conn, void *ptr, size_t maxlen);
+
+void c4rt_connman_server_init(c4rt_conn_server_t *serv, uint32_t port);
+void c4rt_connman_server_deinit(c4rt_conn_server_t *serv);
+c4rt_conn_t *c4rt_connman_server_listen(c4rt_conn_server_t *serv, message_t *msg);
+void c4rt_connman_server_respond(c4rt_conn_t *conn, message_t *msg);
+
+#endif

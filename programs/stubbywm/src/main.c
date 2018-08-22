6becbcc7da7c98ab15260e7e77c3822c872c4a08
My Name <foo@lisp.us.to>
2018-08-21 21:30:09 -0700

    Implementing double buffering in stubbywm

--- programs/stubbywm/src/main.c
+++ programs/stubbywm/src/main.c
@@ -1,4 +1,5 @@
 #include <c4rt/c4rt.h>
+#include <c4rt/mem.h>
 #include <c4rt/interface/peripheral.h>
 #include <c4rt/interface/keyboard.h>
 #include <c4rt/interface/mouse.h>
@@ -19,13 +20,21 @@ typedef struct {
 	unsigned width;
 } stubby_rect_t;
 
+typedef struct window {
+	stubby_rect_t rect;
+
+	int level;
+} window_t;
+
 typedef struct {
 	uint32_t peripherals;
 	stubby_point_t mouse;
 
+	c4_mem_object_t buffer;
+
 	uint32_t buf_cap;
 	uint32_t buf_server;
-	uint8_t *framebuf;
+	uint8_t *framebuffer;
 	framebuffer_info_t info;
 } wm_t;
 
@@ -39,7 +48,7 @@ static inline void draw_pixel(wm_t *state, int32_t x, int32_t y, uint32_t pixel)
 	}
 
 	// TODO: assumes 32 bpp, add support for others
-	uint32_t *buf = (void *)state->framebuf;
+	uint32_t *buf = state->buffer.vaddrptr;
 	buf[(y * state->info.width) + x] = pixel;
 }
 
@@ -58,6 +67,42 @@ static void draw_mouse(wm_t *state, uint32_t color) {
 	r.height = r.width = 16;
 
 	draw_rect(state, &r, color);
+}
+
+static void draw_background(wm_t *state) {
+	for (unsigned y = 0; y < state->info.height; y++) {
+		for (unsigned x = 0; x < state->info.width; x++) {
+			uint32_t pixel = 0x202000 | (x ^ y);
+			draw_pixel(state, x, y, pixel);
+		}
+	}
+}
+
+// TODO: merge these into c4rt at some point, these are pretty useful
+static void page_copy(void *dest, void *src) {
+	uint32_t *a = dest;
+	uint32_t *b = src;
+
+	for (unsigned i = 0; i < PAGE_SIZE / 4; i++) {
+		a[i] = b[i];
+	}
+}
+
+static void pages_copy(void *dest, void *src, unsigned pages) {
+	uint8_t *a = dest;
+	uint8_t *b = src;
+
+	for (unsigned i = 0; i < pages; i++) {
+		page_copy(a, b);
+		a += PAGE_SIZE;
+		b += PAGE_SIZE;
+	}
+}
+
+static void draw_framebuffer(wm_t *state) {
+	unsigned size = (state->info.height * state->info.width * 4);
+	unsigned pages = pager_size_to_pages(size);
+	pages_copy(state->framebuffer, state->buffer.vaddrptr, pages);
 }
 
 static void handle_mouse(wm_t *state, mouse_event_t *ev) {
@@ -80,25 +125,24 @@ static void handle_keyboard(wm_t *state, keyboard_event_t *ev) {
 
 static void event_loop(wm_t *state) {
 	message_t msg;
-	keyboard_event_t kev;
-	mouse_event_t mev;
-
-	draw_mouse(state, 0xff00ff);
+	keyboard_event_t kev = {};
+	mouse_event_t mev = {};
 
 	while (true) {
+		draw_background(state);
+		draw_mouse(state,
+		           0x800080
+		           | ((mev.flags & MOUSE_FLAG_LEFT)?   0xff0000 : 0)
+		           | ((mev.flags & MOUSE_FLAG_RIGHT)?  0x0000ff : 0)
+		           | ((mev.flags & MOUSE_FLAG_MIDDLE)? 0x00ff00 : 0)
+						   );
+		draw_framebuffer(state);
 		c4rt_peripheral_wait_event(&msg, state->peripherals);
 
 		switch (msg.type) {
 			case MOUSE_MSG_EVENT:
-				draw_mouse(state, 0x808080);
 				mouse_parse_event(&msg, &mev);
 				handle_mouse(state, &mev);
-				draw_mouse(state,
-				           0x800080
-				           | ((mev.flags & MOUSE_FLAG_LEFT)?   0xff0000 : 0)
-				           | ((mev.flags & MOUSE_FLAG_RIGHT)?  0x0000ff : 0)
-				           | ((mev.flags & MOUSE_FLAG_MIDDLE)? 0x00ff00 : 0)
-						   );
 				break;
 
 			case KEYBOARD_MSG_EVENT:
@@ -132,8 +176,13 @@ int main(int argc, char *argv[]) {
 	c4_debug_printf("--- stubbywm: cap: %u\n", state.buf_cap);
 	c4_addrspace_map(C4_CURRENT_ADDRSPACE, state.buf_cap,
 	                 0xfb000000, PAGE_READ | PAGE_WRITE);
-	state.framebuf = (void*)0xfb000000;
-	memset(state.framebuf, 0x80, state.info.width * state.info.height * 4);
+	state.framebuffer = (void*)0xfb000000;
+	memset(state.framebuffer, 0x80, state.info.width * state.info.height * 4);
+
+	c4_memobj_alloc(&state.buffer,
+	                state.info.width * state.info.height * 4,
+	                PAGE_READ | PAGE_WRITE);
+	c4_debug_printf("--- stubbywm: have buffer at %p\n", state.buffer.vaddrptr);
 
 	// initialize inputs
 	state.peripherals = c4_msg_create_async();
@@ -147,7 +196,7 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	//c4rt_peripheral_connect(keyboard, state.peripherals);
+	c4rt_peripheral_connect(keyboard, state.peripherals);
 	c4rt_peripheral_connect(mouse, state.peripherals);
 
 	event_loop(&state);
